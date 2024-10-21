8497dcde00ca1ca2fcc0ea037313a25b1505dad6
Kevin P <portillk3@newpaltz.edu>
2024-10-21 10:02:12 -0400

    NavBar changes ( to my liking)

--- Client/src/components/NavBar.vue
+++ Client/src/components/NavBar.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 const isOpen = ref(false)
+const isAccountDropdownOpen = ref(false)
 </script>
 
 <template>
   <nav class="navbar is-info" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="30" height="30" />
+        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="20" height="20" />
 
         <a
           role="button"
@@ -28,29 +30,41 @@ const isOpen = ref(false)
 
       <div class="navbar-menu" :class="{ 'is-active': isOpen }">
         <div class="navbar-start">
-          <RouterLink to="/" class="navbar-item">Home</RouterLink>
-          <RouterLink to="/products" class="navbar-item">Shop</RouterLink>
+          <RouterLink to="/" class="navbar-item">
+            <i class="fas fa-dumbbell mr-2"></i>
+            MyWorkouts
+          </RouterLink>
+
+          <RouterLink to="/" class="navbar-item">
+            <i class="fas fa-chart-area"></i>
+
+            Statistics
+          </RouterLink>
 
           <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link"> More </a>
+            <a class="navbar-link"> <i class="fas fa-users"></i>Friends </a>
 
             <div class="navbar-dropdown">
-              <RouterLink to="/about" class="navbar-item"> About </RouterLink>
-              <RouterLink to="/jobs" class="navbar-item"> Jobs </RouterLink>
-              <RouterLink to="/contact" class="navbar-item"> Contact </RouterLink>
-              <hr class="navbar-divider" />
-              <a class="navbar-item"> Report an issue </a>
+              <RouterLink to="/about" class="navbar-item"> Activity </RouterLink>
+              <RouterLink to="/jobs" class="navbar-item"> Search </RouterLink>
             </div>
           </div>
         </div>
 
         <div class="navbar-end">
           <div class="navbar-item">
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link"> <i class="fas fa-user"></i>Account </a>
+
+              <div class="navbar-dropdown">
+                <RouterLink to="/NewUser" class="navbar-item"> New User </RouterLink>
+                <RouterLink to="/Login" class="navbar-item"> Login </RouterLink>
+              </div>
+            </div>
             <div class="buttons">
-              <a class="button is-primary">
-                <strong>Sign up</strong>
-              </a>
-              <a class="button is-light"> Log in </a>
+              <RouterLink to="/contact" class="button is-primary">
+                <strong>Contact Us </strong>
+              </RouterLink>
             </div>
           </div>
         </div>
@@ -62,6 +76,6 @@ const isOpen = ref(false)
 <style scoped>
 .router-link-active {
   font-weight: bold;
-  border-bottom: 2px solid blue;
+  border-bottom: 2px solid rgb(25, 0, 255);
 }
 </style>
