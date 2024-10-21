46caad4a8d6633231f73f130345e2ab55b7b86ab
Kevin P <portillk3@newpaltz.edu>
2024-10-21 10:55:37 -0400

    changed class to search adn friendsActivity

--- Client/src/pages/Activity.vue
+++ Client/src/pages/Activity.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="friendsActivity">
+    <h1>This is a avtivity page for friends</h1>
   </div>
 </template>
 
--- Client/src/pages/Search.vue
+++ Client/src/pages/Search.vue
@@ -2,9 +2,7 @@
 
 <template>
   <div>
-    <h1 class="title">The Jobs Page</h1>
-    <h2 class="subtitle">This is an example of a jobs page</h2>
-    <p>Here is a list of jobs</p>
+    <h1 class="Search">Search PAge</h1>
   </div>
 </template>
 
