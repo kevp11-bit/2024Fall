83a3e0836f979210079e0327a2776dd559ba1e03
Kevin P <portillk3@newpaltz.edu>
2024-10-21 11:04:34 -0400

    Create an empty Page for Myworkout, Statistics, Friends

---
+++ Client/src/pages/Friends/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="Friends">Friends PAge</h1>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ Client/src/pages/Statistics/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1 class="Statistics">Statistics PAge</h1>
+  </div>
+</template>
+
+<style scoped></style>
